6f3581e9b3fd695d6f36ba9056c2c3973ffaeb2c
Jay Lee <jaylee.possible@gmail.com>
2022-05-26 02:01:50 +0900

    feat: implement feedback generator for ios

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from 'react-native'
 
 const LINKING_ERROR =
   `The package '@m2-modules/react-native-haptic-feedback' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
+  '- You are not using Expo managed workflow\n'
 
 const ReactNativeHapticFeedback = NativeModules.ReactNativeHapticFeedback
   ? NativeModules.ReactNativeHapticFeedback
@@ -12,11 +12,46 @@ const ReactNativeHapticFeedback = NativeModules.ReactNativeHapticFeedback
       {},
       {
         get() {
-          throw new Error(LINKING_ERROR);
+          throw new Error(LINKING_ERROR)
         },
-      }
-    );
+      },
+    )
 
-export function multiply(a: number, b: number): Promise<number> {
-  return ReactNativeHapticFeedback.multiply(a, b);
+const {
+  ImpactFeedbackStyleLight,
+  ImpactFeedbackStyleMedium,
+  ImpactFeedbackStyleHeavy,
+  ImpactFeedbackStyleSoft,
+  ImpactFeedbackStyleRigid,
+  NotificationFeedbackStyleSuccess,
+  NotificationFeedbackStyleWarning,
+  NotificationFeedbackStyleError,
+} = ReactNativeHapticFeedback.getConstants()
+
+export enum ImpactFeedbackStyle {
+  Light = ImpactFeedbackStyleLight,
+  Medium = ImpactFeedbackStyleMedium,
+  Heavy = ImpactFeedbackStyleHeavy,
+  Soft = ImpactFeedbackStyleSoft,
+  Rigid = ImpactFeedbackStyleRigid,
+}
+
+export enum NotificationFeedbackStyle {
+  Success = NotificationFeedbackStyleSuccess,
+  Warning = NotificationFeedbackStyleWarning,
+  Error = NotificationFeedbackStyleError,
+}
+
+export function impactFeedback(impactFeedbackStyle: ImpactFeedbackStyle) {
+  ReactNativeHapticFeedback.impactFeedback(impactFeedbackStyle)
+}
+
+export function selectionFeedback() {
+  ReactNativeHapticFeedback.selectionFeedback()
+}
+
+export function notificationFeedback(
+  notificationFeedbackStyle: NotificationFeedbackStyle,
+) {
+  ReactNativeHapticFeedback.notificationFeedback(notificationFeedbackStyle)
 }
